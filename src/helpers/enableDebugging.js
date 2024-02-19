6c7358905fcb9b3f9962b23d82ce930660eb2b22
Trezy <tre@trezy.com>
2024-02-18 23:34:32 -0600

    fix(helpers): allow `logger` to be a full logging object when enabling debugging

--- src/helpers/enableDebugging.js
+++ src/helpers/enableDebugging.js
@@ -8,12 +8,14 @@ import { state } from './state.js'
 /**
  * Enables debug logging.
  *
- * @param {Console} [logger] The namespace to use for logging.
+ * @param {import('../types/CustomLogger.js').CustomLogger | Function} [logger] The namespace to use for logging.
  */
 export function enableDebugging(logger) {
 	state.isDebuggingEnabled = true
 
 	if (typeof logger === 'function') {
+		state.logger = { log: logger }
+	} else if (typeof logger?.log === 'function') {
 		state.logger = logger
 	} else if (typeof logger !== 'undefined') {
 		throw new TypeError(`\`logger\` must be a function, instead received ${typeof logger}`)
