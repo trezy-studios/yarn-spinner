6044c49d084169bd7346528166e837aede4390f1
Trezy <tre@trezy.com>
2024-02-18 23:37:33 -0600

    fix(helpers): add types for state properties

--- src/helpers/state.js
+++ src/helpers/state.js
@@ -1,4 +1,7 @@
 export const state = {
+	/** @type {boolean} */
 	isDebuggingEnabled: false,
+
+	/** @type {import('../types/CustomLogger.js').CustomLogger} */
 	logger: console,
 }
---
+++ src/types/CustomLogger.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {object} CustomLogger
+ * @property {Function} [error] Log error to the console.
+ * @property {Function} [group] Start grouping logs.
+ * @property {Function} [groupEnd] Stop grouping logs.
+ * @property {Function} [info] Log to the console.
+ * @property {Function} log Log to the console.
+ * @property {Function} [warn] Log warning to the console.
+ */
+export const CustomLogger = {}
