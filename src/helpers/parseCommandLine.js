2993a3b1aad5cae776a522ac1e68983b12b11309
Trezy <tre@trezy.com>
2024-02-18 23:35:41 -0600

    fix(helpers): properly parse non-string command parameters

--- src/helpers/parseCommandLine.js
+++ src/helpers/parseCommandLine.js
@@ -5,10 +5,28 @@
  * @returns {object} The parsed line.
  */
 export function parseCommandLine(line) {
-	const [, commandName, parameters] = /^\s*<<(\w+)(?:\s(.*))?>>\s*$/u.exec(line.trim())
+	const [,
+		commandName,
+		parameters = '',
+	] = /^\s*<<(\w+)(?:\s(.*))?>>\s*$/u.exec(line.trim())
+
+	const parsedParameters = parameters
+		.split(' ')
+		.filter(Boolean)
+		.map(parameter => {
+			if (!isNaN(parseFloat(parameter))) {
+				return parseFloat(parameter)
+			}
+
+			if (['false', 'true'].includes(parameter)) {
+				return Boolean(parameter)
+			}
+
+			return parameter
+		})
 
 	return {
 		commandName,
-		parameters: parameters.split(' '),
+		parameters: parsedParameters,
 	}
 }
